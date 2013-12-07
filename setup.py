0c92a7e0da885fd2ad89468d10c59afebb8cbc88
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-12-07 15:35:11 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='traitlets',
-    version='0.0.1',
+    version='0.0.2',
     author='IPython dev team',
     url='https://github.com/ipython/traitlets',
     packages=['traitlets',],
