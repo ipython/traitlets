757bb592e87f2b0e9c43a000864ce61bb83ff746
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-12-07 15:25:35 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 
 setup(
     name='traitlets',
-    version='0.1-dev',
+    version='0.0.1',
+    author='IPython dev team',
+    url='https://github.com/ipython/traitlets',
     packages=['traitlets',],
     license='BSD',
     long_description=open('README.md').read(),
