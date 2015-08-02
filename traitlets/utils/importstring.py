49b28e3b4c63af0b1b262a09b73ccbdbfa9a75c6
Scott Sanderson <scoutoss@gmail.com>
2015-08-02 17:13:09 -0400

    DEV: Handle both string types in import_item.
    - In PY2, coerce unicode to bytes.
    - In PY3, coerce bytes to unicode.
    - Add tests for the above behavior.

--- traitlets/utils/importstring.py
+++ traitlets/utils/importstring.py
@@ -2,9 +2,35 @@
 """
 A simple utility to import something by its string name.
 """
-
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
+from sys import version_info
+
+
+def _check_stringlike(s, types):
+    if not isinstance(s, types):
+        raise TypeError(
+            "import_item expected input of type "
+            "{0[0]} or {0[1]}, but got '{1}' instead.".format(
+                types, type(s),
+            )
+        )
+
+
+if version_info.major == 2:
+    # PY2 __import__ expects bytes
+    def _to_module_str(s):
+        _check_stringlike(s, (bytes, unicode))
+        if isinstance(s, unicode):
+            s = s.encode('utf-8')
+        return s
+elif version_info.major == 3:
+    # PY3 __import__ expects unicode
+    def _to_module_str(s):
+        _check_stringlike(s, (bytes, str))
+        if isinstance(s, bytes):
+            s = s.decode('utf-8')
+        return s
 
 
 def import_item(name):
@@ -23,7 +49,7 @@ def import_item(name):
     mod : module object
        The module that was imported.
     """
-    
+    name = _to_module_str(name)
     parts = name.rsplit('.', 1)
     if len(parts) == 2:
         # called with 'foo.bar....'
