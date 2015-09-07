4c9e4fe8f6108133b8436ab87ff989d84f006c41
Min RK <benjaminrk@gmail.com>
2015-09-07 13:34:18 +0200

    Cast unicode module name to str on Python 2

--- traitlets/utils/importstring.py
+++ traitlets/utils/importstring.py
@@ -4,34 +4,8 @@ A simple utility to import something by its string name.
 """
 # Copyright (c) IPython Development Team.
 # Distributed under the terms of the Modified BSD License.
-from sys import version_info
 
-
-def _check_stringlike(s, types):
-    if not isinstance(s, types):
-        raise TypeError(
-            "import_item expected input of type "
-            "{0[0]} or {0[1]}, but got '{1}' instead.".format(
-                types, type(s),
-            )
-        )
-
-
-if version_info.major == 2:
-    # PY2 __import__ expects bytes
-    def _to_module_str(s):
-        _check_stringlike(s, (bytes, unicode))
-        if isinstance(s, unicode):
-            s = s.encode('utf-8')
-        return s
-elif version_info.major == 3:
-    # PY3 __import__ expects unicode
-    def _to_module_str(s):
-        _check_stringlike(s, (bytes, str))
-        if isinstance(s, bytes):
-            s = s.decode('utf-8')
-        return s
-
+from ipython_genutils.py3compat import cast_bytes_py2
 
 def import_item(name):
     """Import and return ``bar`` given the string ``foo.bar``.
@@ -49,7 +23,7 @@ def import_item(name):
     mod : module object
        The module that was imported.
     """
-    name = _to_module_str(name)
+    name = cast_bytes_py2(name)
     parts = name.rsplit('.', 1)
     if len(parts) == 2:
         # called with 'foo.bar....'
