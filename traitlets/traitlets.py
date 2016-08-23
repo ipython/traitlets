8dfc8d90c0588058dc52a01473a6e3ba849b0c05
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-08-23 12:51:51 +0200

    Change dict must have a name key

--- traitlets/traitlets.py
+++ traitlets/traitlets.py
@@ -768,8 +768,9 @@ def observe(*names, **kwargs):
     """A decorator which can be used to observe Traits on a class.
 
     The handler passed to the decorator will be called with one ``change``
-    dict argument. The change dictionary at least holds a 'type' key,
-    corresponding to the type of notification.
+    dict argument. The change dictionary at least holds a 'type' key and a
+    'name' key, corresponding respectively to the type of notification and the
+    name of the attribute that triggered the notification.
 
     Other keys may be passed depending on the value of 'type'. In the case
     where type is 'change', we also have the following keys:
