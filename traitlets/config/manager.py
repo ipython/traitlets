06a3a38efa80029be8351821697acb86a516efac
maartenbreddels <maartenbreddels@gmail.com>
2017-09-01 15:15:08 +0200

    allow default json files in a .d directory

--- traitlets/config/manager.py
+++ traitlets/config/manager.py
@@ -9,7 +9,7 @@ import os
 
 from six import PY3
 from traitlets.config import LoggingConfigurable
-from traitlets.traitlets import Unicode
+from traitlets.traitlets import Unicode, Bool
 
 
 def recursive_update(target, new):
@@ -36,10 +36,12 @@ def recursive_update(target, new):
 class BaseJSONConfigManager(LoggingConfigurable):
     """General JSON config manager
     
-    Deals with persisting/storing config in a json file
+    Deals with persisting/storing config in a json file with optionally
+    default values in a {section_name}.d directory.
     """
 
     config_dir = Unicode('.')
+    read_directory = Bool(True)
 
     def ensure_config_dir_exists(self):
         try:
@@ -51,6 +53,9 @@ class BaseJSONConfigManager(LoggingConfigurable):
     def file_name(self, section_name):
         return os.path.join(self.config_dir, section_name+'.json')
 
+    def directory(self, section_name):
+        return os.path.join(self.config_dir, section_name+'.d')
+
     def get(self, section_name):
         """Retrieve the config data for the specified section.
 
@@ -58,11 +63,23 @@ class BaseJSONConfigManager(LoggingConfigurable):
         doesn't exist.
         """
         filename = self.file_name(section_name)
+        data = {}
+        if self.read_directory:
+            directory = self.directory(section_name)
+            if os.path.isdir(directory):
+                paths = os.listdir(directory)
+                for path in sorted(paths):
+                    name, ext = os.path.splitext(path)
+                    abspath = os.path.join(directory, path)
+                    if ext == ".json" and name not in data:
+                        with io.open(abspath, encoding='utf-8') as f:
+                            more_defaults = json.load(f)
+                        recursive_update(data, more_defaults)
         if os.path.isfile(filename):
             with io.open(filename, encoding='utf-8') as f:
-                return json.load(f)
-        else:
-            return {}
+                new_data = json.load(f)
+            recursive_update(data, new_data)
+        return data
 
     def set(self, section_name, data):
         """Store the given config data.
