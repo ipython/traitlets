6904ad15c70be2ba0dfda87a621fb92b26240576
Ryan Morshead <ryan.morshead@gmail.com>
2018-02-28 22:02:30 -0800

    fix various issues with describe

--- traitlets/utils/descriptions.py
+++ traitlets/utils/descriptions.py
@@ -11,8 +11,8 @@ def describe(article, value, name=None, verbose=False, capital=False):
     ----------
     article: str or None
         A definite or indefinite article. If the article is
-        indefinite (i.e. "a" or "an") the appropriate one
-        will be infered. Thus, the arguments of ``describe``
+        indefinite (i.e. ends with "a" or "an") the appropriate
+        one will be infered. Thus, the arguments of ``describe``
         can themselves represent what the resulting string
         will actually look like. If None, then no article
         will be prepended to the result. For non-articled
@@ -20,7 +20,7 @@ def describe(article, value, name=None, verbose=False, capital=False):
         definitely, while classes are handled indefinitely.
     value: any
         The value which will be named.
-    name: str or None (default: None)
+    name: str, or None (default: None)
         Only applies when ``article`` is "the" - this
         ``name`` is a definite reference to the value.
         By default one will be infered from the value's
@@ -35,18 +35,15 @@ def describe(article, value, name=None, verbose=False, capital=False):
 
     Examples
     --------
-
-    Indefinite description:
-
+    **Indefinite description:**
     >>> describe("a", object())
     'an object'
     >>> describe("a", object)
     'an object'
     >>> describe("a", type(object))
     'a type'
-    
-    Definite description:
 
+    **Definite description:**
     >>> describe("the", object())
     "the object at '0x10741f1b0'"
     >>> describe("the", object)
@@ -54,12 +51,15 @@ def describe(article, value, name=None, verbose=False, capital=False):
     >>> describe("the", type(object))
     "the type 'type'"
 
-    Definitely named description:
-
+    **Definitely named description:**
     >>> describe("the", object(), "I made")
     'the object I made'
     >>> describe("the", object, "I will use")
     'the object I will use'
+
+    **Arbitrarily articled description:**
+    >>> describe("the element of a", object())
+    'the element of an object'
     """
     if isinstance(article, str):
         article = article.lower()
@@ -71,59 +71,25 @@ def describe(article, value, name=None, verbose=False, capital=False):
     if verbose:
         typename = _prefix(value) + typename
 
-    if article == "the" or (article is None and not inspect.isclass(value)):
-        if name is not None:
-            result = "%s %s" % (typename, name)
-            if article is not None:
-                return add_article(result, True, capital)
-            else:
-                return result
+    if article is None:
+        article = "the" if inspect.isclass(value) else "a"
+        temp = describe(article, value, name, verbose)
+        final = temp.split(" ", 1)[1]
+    elif not _indefinite(article):
+        if name is None:
+            final = describe(article, type(value), _name(value, verbose), verbose)
         else:
-            tick_wrap = False
-            if inspect.isclass(value):
-                name = value.__name__
-            elif isinstance(value, types.FunctionType):
-                name = value.__name__
-                tick_wrap = True
-            elif isinstance(value, types.MethodType):
-                name = value.__func__.__name__
-                tick_wrap = True
-            elif type(value).__repr__ in (object.__repr__, type.__repr__):
-                name = "at '%s'" % hex(id(value))
-                verbose = False
-            else:
-                name = repr(value)
-                verbose = False
-            if verbose:
-                name = _prefix(value) + name
-            if tick_wrap:
-                name = name.join("''")
-            return describe(article, value, name=name,
-                verbose=verbose, capital=capital)
-    elif article in ("a", "an") or article is None:
-        if article is None:
-            return typename
-        return add_article(typename, False, capital)
+            final = _articled(article, "%s %s" % (typename, name))
     else:
-        raise ValueError("The 'article' argument should "
-            "be 'the', 'a', 'an', or None not %r" % article)
-
-    
-def _prefix(value):
-    if isinstance(value, types.MethodType):
-        name = describe(None, value.__self__, verbose=True) + '.'
-    else:
-        module = inspect.getmodule(value)
-        if module is not None and module.__name__ != "builtins":
-            name = module.__name__ + '.'
-        else:
-            name = ""
-    return name
+        final = _articled(article, typename + (
+            "" if name is None else " " + name))
+    if capital:
+        final = final[:1].upper() + final[1:]
+    return final.strip()
 
 
 def class_of(value):
     """Returns a string of the value's type with an indefinite article.
-
     For example 'an Image' or 'a PlotValue'.
     """
     if inspect.isclass(value):
@@ -134,8 +100,6 @@ def class_of(value):
 
 def add_article(name, definite=False, capital=False):
     """Returns the string with a prepended article.
-
-    The input does not need to begin with a charater.
 
     Parameters
     ----------
@@ -150,8 +114,7 @@ def add_article(name, definite=False, capital=False):
     if definite:
         result = "the " + name
     else:
-        first_letters = re.compile(r'[\W_]+').sub('', name)
-        if first_letters[:1].lower() in 'aeiou':
+        if name[:1].lower() in 'aeiou':
             result = 'an ' + name
         else:
             result = 'a ' + name
@@ -164,7 +127,6 @@ def add_article(name, definite=False, capital=False):
 
 def repr_type(obj):
     """Return a string representation of a value and its type for readable
-
     error messages.
     """
     the_type = type(obj)
@@ -173,3 +135,63 @@ def repr_type(obj):
         the_type = obj.__class__
     msg = '%r %r' % (obj, the_type)
     return msg
+
+
+def _articled(article, value):
+    if article == "a" or article.endswith(" a"):
+        if value[:1] in "aeiou":
+            article = article[:-1] + "an"
+    elif article == "an" or article.endswith(" an"):
+        if value[:1] not in "aeiou":
+            article = article[:-2] + "a"
+    return "%s %s" % (article, value)
+
+
+def _indefinite(article):
+    for char in ("a", "an"):
+        if article == char or article.endswith(" " + char):
+            return True
+    else:
+        if (article == "the" or
+            article.endswith(" the") or
+            article.startswith("the ")):
+            return False
+        else:
+            raise ValueError("Expected an indefinite article string that "
+                "ends in 'a' or 'an', or a definite article that starts or "
+                "ends with 'the', not %s" % describe("the", article))
+
+
+def _name(value, verbose):
+    tick_wrap = False
+    if inspect.isclass(value):
+        name = value.__name__
+    elif isinstance(value, types.FunctionType):
+        name = value.__name__
+        tick_wrap = True
+    elif isinstance(value, types.MethodType):
+        name = value.__func__.__name__
+        tick_wrap = True
+    elif type(value).__repr__ in (object.__repr__, type.__repr__):
+        name = "at '%s'" % hex(id(value))
+        verbose = False
+    else:
+        name = repr(value)
+        verbose = False
+    if verbose:
+        name = _prefix(value) + name
+    if tick_wrap:
+        name = name.join("''")
+    return name
+
+
+def _prefix(value):
+    if isinstance(value, types.MethodType):
+        name = describe(None, value.__self__, verbose=True) + '.'
+    else:
+        module = inspect.getmodule(value)
+        if module is not None and module.__name__ != "builtins":
+            name = module.__name__ + '.'
+        else:
+            name = ""
+    return name
