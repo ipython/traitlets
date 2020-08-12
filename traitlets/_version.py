a362e91fb9a92dc6b1842ccdd9146ff218e8dec8
Matthias Bussonnier <bussonniermatthias@gmail.com>
2020-08-12 13:47:10 -0700

    Prepare for an beta1 release.
    Remove universal wheel (we are Python 3 only anyway)
    Bump version with beta1, be careful about the format (with or without
    dots)

--- traitlets/_version.py
+++ traitlets/_version.py
@@ -1,2 +1,14 @@
-version_info = (5, 0, 0, 'dev')
-__version__ = '.'.join(map(str, version_info))
+version_info = (5, 0, 0, "b1")
+
+# unlike `.dev`, alpha, beta and rc _must not_ have dots,
+# or the wheel and tgz won't look to pip like the same version.
+
+__version__ = (
+    ".".join(map(str, version_info))
+    .replace(".b", "b")
+    .replace(".a", "a")
+    .replace(".rc", "rc")
+)
+assert ".b" not in __version__
+assert ".a" not in __version__
+assert ".rc" not in __version__
