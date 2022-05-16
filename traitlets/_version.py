d566cd8480b58a3fbc46b1e2a9a52c7d53084ea4
Steven Silvester <steven.silvester@ieee.org>
2022-05-16 06:25:54 -0500

    switch to hatch backend

--- traitlets/_version.py
+++ traitlets/_version.py
@@ -1,9 +1,10 @@
 version_info = (5, 3, 0, "dev0")
+__version__ = "5.3.0.dev0"
 
 # unlike `.dev`, alpha, beta and rc _must not_ have dots,
 # or the wheel and tgz won't look to pip like the same version.
 
-__version__ = (
+assert __version__ == (
     ".".join(map(str, version_info)).replace(".b", "b").replace(".a", "a").replace(".rc", "rc")
 )
 assert ".b" not in __version__
