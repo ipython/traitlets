e6055389f6e317cf9bb9a4855b5a2e9c29878bc0
Min RK <benjaminrk@gmail.com>
2022-06-17 14:31:14 +0200

    parse version_info from __version__

--- traitlets/_version.py
+++ traitlets/_version.py
@@ -1,12 +1,20 @@
-version_info = (5, 4, 0, "dev0")
+import re
+
 __version__ = "5.4.0.dev0"
 
-# unlike `.dev`, alpha, beta and rc _must not_ have dots,
-# or the wheel and tgz won't look to pip like the same version.
 
-assert __version__ == (
-    ".".join(map(str, version_info)).replace(".b", "b").replace(".a", "a").replace(".rc", "rc")
+# light version of the pep440 subset we use
+# to build version_info tuple
+_match = re.match(
+    r"(\d+)+\.(\d+)\.(\d+)((?:(a|b|rc)\d+)|(?:\.dev\d+)|)$",
+    __version__,
 )
-assert ".b" not in __version__
-assert ".a" not in __version__
-assert ".rc" not in __version__
+assert _match is not None, f"Invalid __version__: {__version__}"
+
+_v = _match.groups()
+version_info = tuple(int(_v[i]) for i in range(3))
+if _v[3]:
+    # item 3 is _any_ extra info on the end (pre, .post, etc.)
+    version_info = version_info + (_v[3],)
+
+print(version_info)
