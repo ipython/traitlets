f294990bca8e9ebd76f0848de5d551cd341668b4
azhu <azhu@tower-research.com>
2022-12-16 11:40:10 +1300

    Try to bisect towards argcomplete tests

--- traitlets/tests/test_tempfile.py
+++ traitlets/tests/test_tempfile.py
@@ -1,9 +1,31 @@
+import os
 from pathlib import Path
+import sys
 from tempfile import TemporaryFile, TemporaryDirectory
 
+import pytest
 
 class TestTempFile:
-    def test_temp_file1(self):
+    IFS = "\013"
+    COMP_WORDBREAKS = " \t\n\"'><=;|&(:"
+
+    @pytest.fixture
+    def argcomplete_on(self):
+        """Mostly borrowed from argcomplete's unit test fixtures
+
+        Set up environment variables to mimic those passed by argcomplete
+        """
+        _old_environ = os.environ
+        os.environ = os.environ.copy()  # type: ignore[assignment]
+        os.environ["_ARGCOMPLETE"] = "1"
+        os.environ["_ARC_DEBUG"] = "yes"
+        os.environ["IFS"] = self.IFS
+        os.environ["_ARGCOMPLETE_COMP_WORDBREAKS"] = self.COMP_WORDBREAKS
+        os.environ["_ARGCOMPLETE"] = "1"
+        yield
+        os.environ = _old_environ
+
+    def test_temp_file1(self, argcomplete_on):
         with TemporaryFile("wt+") as t:
             t.write("hello world")
             t.flush()
@@ -15,39 +37,49 @@ class TestTempFile:
             t.seek(0)
             assert t.read() == "hello world"
 
-    def test_temp_file2(self):
+    def test_temp_file2(self, argcomplete_on):
         with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
             t.write("hello world")
             t.flush()
             t.seek(0)
             assert t.read() == "hello world"
 
         with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
             t.write("hello world")
             t.flush()
             t.seek(0)
             assert t.read() == "hello world"
 
         with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
             t.write("hello world")
             t.flush()
             t.seek(0)
             assert t.read() == "hello world"
 
-    def test_temp_file3(self):
+    def test_temp_file3(self, argcomplete_on):
         with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
             t.write("hello world")
             t.flush()
             t.seek(0)
             assert t.read() == "hello world"
 
         with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
             t.write("hello world")
             t.flush()
             t.seek(0)
             assert t.read() == "hello world"
 
-    def test_temp_dir1(self):
+    def test_temp_dir1(self, argcomplete_on):
         with TemporaryDirectory() as tmpdir:
             with Path(tmpdir).joinpath("tmp").open("wt+") as t:
                 t.write("hello world")
@@ -55,7 +87,7 @@ class TestTempFile:
                 t.seek(0)
                 assert t.read() == "hello world"
 
-    def test_temp_dir2(self):
+    def test_temp_dir2(self, argcomplete_on):
         with TemporaryDirectory() as tmpdir:
             with Path(tmpdir).joinpath("tmp").open("wt+") as t:
                 t.write("hello world")
@@ -63,7 +95,7 @@ class TestTempFile:
                 t.seek(0)
                 assert t.read() == "hello world"
 
-    def test_temp_dir3(self):
+    def test_temp_dir3(self, argcomplete_on):
         with TemporaryDirectory() as tmpdir:
             with Path(tmpdir).joinpath("tmp").open("wt+") as t:
                 t.write("hello world")
