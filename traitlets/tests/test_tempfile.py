b08e36c983738e84b0557792fd244e85a7ed8c43
azhu <azhu@tower-research.com>
2022-12-17 11:11:39 +1300

    MRE attempt failed, restoring

--- traitlets/tests/test_tempfile.py
+++ traitlets/tests/test_tempfile.py
@@ -1,183 +1,151 @@
+import contextlib
 import os
 import sys
+from pathlib import Path
+from tempfile import TemporaryFile, TemporaryDirectory
+
 import pytest
+
+_DEBUG = True
+
+
+@contextlib.contextmanager
+def mute_stdout():
+    stdout = sys.stdout
+    sys.stdout = open(os.devnull, "w")
+    try:
+        yield
+    finally:
+        sys.stdout = stdout
+
+
+@contextlib.contextmanager
+def mute_stderr():
+    stderr = sys.stderr
+    sys.stderr = open(os.devnull, "w")
+    try:
+        yield
+    finally:
+        sys.stderr.close()
+        sys.stderr = stderr
+
 
 debug_stream = sys.stderr
 
 
-@pytest.fixture
-def argcomplete_on():
+def debug(*args):
+    if _DEBUG:
+        print(file=debug_stream, *args)
+
+
+class TestTempFile:
     IFS = "\013"
     COMP_WORDBREAKS = " \t\n\"'><=;|&(:"
 
-    _old_environ = os.environ
-    os.environ = os.environ.copy()  # type: ignore[assignment]
-    os.environ["_ARGCOMPLETE"] = "1"
-    os.environ["_ARC_DEBUG"] = "yes"
-    os.environ["IFS"] = IFS
-    os.environ["_ARGCOMPLETE_COMP_WORDBREAKS"] = COMP_WORDBREAKS
-    os.environ["_ARGCOMPLETE"] = "1"
-    yield
-    os.environ = _old_environ
+    @pytest.fixture
+    def argcomplete_on(self):
+        """Mostly borrowed from argcomplete's unit test fixtures
 
-def test_write_debug(argcomplete_on):
-    global debug_stream
-    try:
-        debug_stream = os.fdopen(9, "w")
-    except Exception:
-        debug_stream = sys.stderr
-    print(file=debug_stream)
-    debug_stream.flush()
+        Set up environment variables to mimic those passed by argcomplete
+        """
+        global debug_stream
+        try:
+            debug_stream = os.fdopen(9, "w")
+        except Exception:
+            debug_stream = sys.stderr
+        debug("test")
+        debug_stream.flush()
 
+        # with mute_stdout():
+        #     print("abc")
 
-# import contextlib
-# import os
-# import sys
-# from pathlib import Path
-# from tempfile import TemporaryFile, TemporaryDirectory
-#
-# import pytest
-#
-# _DEBUG = True
-#
-#
-# @contextlib.contextmanager
-# def mute_stdout():
-#     stdout = sys.stdout
-#     sys.stdout = open(os.devnull, "w")
-#     try:
-#         yield
-#     finally:
-#         sys.stdout = stdout
-#
-#
-# @contextlib.contextmanager
-# def mute_stderr():
-#     stderr = sys.stderr
-#     sys.stderr = open(os.devnull, "w")
-#     try:
-#         yield
-#     finally:
-#         sys.stderr.close()
-#         sys.stderr = stderr
-#
-#
-# debug_stream = sys.stderr
-#
-#
-# def debug(*args):
-#     if _DEBUG:
-#         print(file=debug_stream, *args)
-#
-#
-# class TestTempFile:
-#     IFS = "\013"
-#     COMP_WORDBREAKS = " \t\n\"'><=;|&(:"
-#
-#     @pytest.fixture
-#     def argcomplete_on(self):
-#         """Mostly borrowed from argcomplete's unit test fixtures
-#
-#         Set up environment variables to mimic those passed by argcomplete
-#         """
-#         global debug_stream
-#         try:
-#             debug_stream = os.fdopen(9, "w")
-#         except Exception:
-#             debug_stream = sys.stderr
-#         debug("test")
-#         debug_stream.flush()
-#
-#         # with mute_stdout():
-#         #     print("abc")
-#
-#         # with mute_stderr():
-#         #     print("def", file=sys.stderr)
-#
-#         _old_environ = os.environ
-#         os.environ = os.environ.copy()  # type: ignore[assignment]
-#         os.environ["_ARGCOMPLETE"] = "1"
-#         os.environ["_ARC_DEBUG"] = "yes"
-#         os.environ["IFS"] = self.IFS
-#         os.environ["_ARGCOMPLETE_COMP_WORDBREAKS"] = self.COMP_WORDBREAKS
-#         os.environ["_ARGCOMPLETE"] = "1"
-#         yield
-#         os.environ = _old_environ
-#
-#     def test_temp_file1(self, argcomplete_on):
-#         with TemporaryFile("wt+") as t:
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#         with TemporaryFile("wt+") as t:
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#
-#     def test_temp_file2(self, argcomplete_on):
-#         with TemporaryFile("wt+") as t:
-#             with pytest.raises(SystemExit):
-#                 sys.exit(0)
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#
-#         with TemporaryFile("wt+") as t:
-#             with pytest.raises(SystemExit):
-#                 sys.exit(0)
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#
-#         with TemporaryFile("wt+") as t:
-#             with pytest.raises(SystemExit):
-#                 sys.exit(0)
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#
-#     def test_temp_file3(self, argcomplete_on):
-#         with TemporaryFile("wt+") as t:
-#             with pytest.raises(SystemExit):
-#                 sys.exit(0)
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#
-#         with TemporaryFile("wt+") as t:
-#             with pytest.raises(SystemExit):
-#                 sys.exit(0)
-#             t.write("hello world")
-#             t.flush()
-#             t.seek(0)
-#             assert t.read() == "hello world"
-#
-#     def test_temp_dir1(self, argcomplete_on):
-#         with TemporaryDirectory() as tmpdir:
-#             with Path(tmpdir).joinpath("tmp").open("wt+") as t:
-#                 t.write("hello world")
-#                 t.flush()
-#                 t.seek(0)
-#                 assert t.read() == "hello world"
-#
-#     def test_temp_dir2(self, argcomplete_on):
-#         with TemporaryDirectory() as tmpdir:
-#             with Path(tmpdir).joinpath("tmp").open("wt+") as t:
-#                 t.write("hello world")
-#                 t.flush()
-#                 t.seek(0)
-#                 assert t.read() == "hello world"
-#
-#     def test_temp_dir3(self, argcomplete_on):
-#         with TemporaryDirectory() as tmpdir:
-#             with Path(tmpdir).joinpath("tmp").open("wt+") as t:
-#                 t.write("hello world")
-#                 t.flush()
-#                 t.seek(0)
-#                 assert t.read() == "hello world"
+        # with mute_stderr():
+        #     print("def", file=sys.stderr)
+
+        _old_environ = os.environ
+        os.environ = os.environ.copy()  # type: ignore[assignment]
+        os.environ["_ARGCOMPLETE"] = "1"
+        os.environ["_ARC_DEBUG"] = "yes"
+        os.environ["IFS"] = self.IFS
+        os.environ["_ARGCOMPLETE_COMP_WORDBREAKS"] = self.COMP_WORDBREAKS
+        os.environ["_ARGCOMPLETE"] = "1"
+        yield
+        os.environ = _old_environ
+
+    def test_temp_file1(self, argcomplete_on):
+        with TemporaryFile("wt+") as t:
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+        with TemporaryFile("wt+") as t:
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+
+    def test_temp_file2(self, argcomplete_on):
+        with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+
+        with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+
+        with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+
+    def test_temp_file3(self, argcomplete_on):
+        with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+
+        with TemporaryFile("wt+") as t:
+            with pytest.raises(SystemExit):
+                sys.exit(0)
+            t.write("hello world")
+            t.flush()
+            t.seek(0)
+            assert t.read() == "hello world"
+
+    def test_temp_dir1(self, argcomplete_on):
+        with TemporaryDirectory() as tmpdir:
+            with Path(tmpdir).joinpath("tmp").open("wt+") as t:
+                t.write("hello world")
+                t.flush()
+                t.seek(0)
+                assert t.read() == "hello world"
+
+    def test_temp_dir2(self, argcomplete_on):
+        with TemporaryDirectory() as tmpdir:
+            with Path(tmpdir).joinpath("tmp").open("wt+") as t:
+                t.write("hello world")
+                t.flush()
+                t.seek(0)
+                assert t.read() == "hello world"
+
+    def test_temp_dir3(self, argcomplete_on):
+        with TemporaryDirectory() as tmpdir:
+            with Path(tmpdir).joinpath("tmp").open("wt+") as t:
+                t.write("hello world")
+                t.flush()
+                t.seek(0)
+                assert t.read() == "hello world"
