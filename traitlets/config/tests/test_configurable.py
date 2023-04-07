36c5c827772125bb6f050f42084193a8f17b8199
Steven Silvester <steven.silvester@ieee.org>
2023-04-07 06:40:05 -0500

    fix import

--- traitlets/config/tests/test_configurable.py
+++ traitlets/config/tests/test_configurable.py
@@ -22,9 +22,9 @@ from traitlets.traitlets import (
     List,
     Set,
     Unicode,
-    _deprecations_shown,
     validate,
 )
+from traitlets.utils.warnings import _deprecations_shown
 
 from ...tests._warnings import expected_warnings
 
